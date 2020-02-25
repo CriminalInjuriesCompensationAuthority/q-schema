00f8f0fc404975ac032302d722cd444c6e597852
Stephen S <stephen.sinclair@cica.gov.uk>
2020-02-25 13:03:23 +0000

    Add questionnaire schema v1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Questionnaire schema
+
+A JSON Schema (https://json-schema.org/) for validating a questionnaire template structure.
--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,8 @@ const config = {
 };
 /*! m0-end */
 
+config.setupFilesAfterEnv = ['jest-extended'];
+
 /*! m0-start */
 module.exports = config;
 /*! m0-end */
